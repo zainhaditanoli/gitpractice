a4f8c54669acc99c33e18651a89c6e3aee10b1cd
Lab-1 <muhammadanasasif3@gmail.com>
2023-03-04 08:32:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+// var a=prompt("Enter name:")
+// var l=[]
+// for (let index = 0; index < a.length; index++) {
+//    if (a[index]===" ") {
+//     l.push(a[index+1])
+//    }
+    
+// }
+// console.log(a[0])
+// for (let index = 0; index < l.length; index++) {
+//     console.log(l[index])
+// }
+// var s=a[0]
+// for (let index = 0; index < a.length; index++) {
+//     if (a[index]===" ") {
+//         s+=" "+a[index+1]
+//     }   
+// }
+// console.log(s) 
+// for (let index = 0; index < a.length; index++) {
+//    if (index+1!=a[index]) {
+//   console.log(index+1)
+//   break
+// }   
+// }
+// var a=[1,2,3,4,5,7,8,9,10]
+// var b
+// for (let index = 0; index < a.length-1; index++) {
+//     var c=a[index]+1
+//     if (c!=a[index+1]) {
+//         b=c;
+//         console.log(b)
+//     }   
+//  }
+//  var arr=[2,8,7,6,4,1,3,50,44]
+//  var lar=0
+//  var smal=arr[0]
+//  for (let index = 0; index < arr.length; index++) {
+//    if (arr[index]>lar) {
+//     lar=arr[index]
+//    }
+//    if (arr[index]<smal) {
+//     smal=arr[index]
+//    }
+    
+//  }
+//  console.log('largest = '+lar+" small = "+smal)
+
+// let a = 4 ** 2
+
+// console.log(a)
+
+// a += 2
+// console.log(a)
+
+// a -= 2 
+// console.log(a)
+
+// a *= 2 
+// console.log(a)
+
+// a /= 2 
+// console.log(a)
+
+// a **= 5
+// console.log(a)
+
+// let b = +prompt("Enter your age")
+
+// if (b < 18){
+//   console.log("you cannot drive")
+// } else {
+//   console.log("you can drive")
+// }
+
+// if (b % 2 == 0 && b % 3 == 0){
+//   console.log("divisible by 2 & 3")
+// } else if (b % 2 == 0){
+//   console.log("divisible by 2")
+// } else if (b % 3 == 0){
+//   console.log("divisible by 3")
+// }
+
+// if (b >= 10 && b <= 20){
+//   console.log("you are fit")
+// }
+
+// const a = "my upwork id is here"
+
+// for (var i = 0; i < a.length; i++){
+//   if (a.slice(i, i + 6) === "upwork"){
+//     alert("please don't use letter upwork")
+//   }
+// }
+
+// const b = "my freelancer id is here"
+
+// for (var i = 0; i < b.length; i++){
+//   if (b.slice(i, i + 10) === "freelancer"){
+//     alert("please don't use letter " + b.slice(i, i + 10))
+//   }
+// }
